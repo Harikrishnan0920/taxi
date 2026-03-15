dde46bcbd88b25579279efa983e0d2d27877aabf
thatonedeveloperguy <Harikrishna3@>
2026-03-15 15:09:41 +0530

    render fix 1

--- server-side/index.js
+++ server-side/index.js
@@ -77,5 +77,11 @@ io.on("connection", (socket) => {
 
 app.use("/api", routes);
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
+
 
 export default app;
